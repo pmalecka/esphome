e029924b3892fc5a2fa4e2dc0b6519e04902aa7f
Peter Malecka <peter.malecka@outlook.com>
2025-03-02 16:29:22 +0000

    bugfix in an unimplemented restart button

--- esphome/components/fully_jarvis_f/button/restart_button.cpp
+++ esphome/components/fully_jarvis_f/button/restart_button.cpp
@@ -5,7 +5,7 @@ namespace fully_jarvis_f {
 // TODO: a setting like this could be beneficial as a diagnostic thing.. to reset the
 //       state machine to initial state and start requesting settings again.. (just an idea)
 
-void RestartButton::press_action() { this->parent_->restart_and_read_all_info(); }
+void RestartButton::press_action() { this->parent_->read_all_info(); }
 
 }  // namespace fully_jarvis_f
 }  // namespace esphome
